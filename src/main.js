d20bd84cc125a8045fe20239ceffc9a43269241b
drkchiloll <samwomack_@me.com>
2015-09-04 10:28:18 -0500

    debug/test actions.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
-var events = require('events');
-console.log('hello world');
-console.log(events);
+var actions = require('./actions');
+
+Object.keys(actions).forEach(function(key) {
+  console.log(key);
+  console.log(actions[key].toString());
+});
