b61b01595ee73a69b10a6717b78bba5a315b6ae3
drkchiloll <samwomack_@me.com>
2015-09-05 12:55:13 -0500

    Added UserProfile Page

---
+++ src/components/UserProfile.js
@@ -0,0 +1,49 @@
+var React = require('react'),
+    UserStore = require('../stores/users'),
+    ChirpStore = require('../stores/chirps'),
+    utils = require('../utils'),
+    FollowButton = require('./FollowButton');
+
+var UserProfile = module.exports = React.createClass({
+  getInitialState : function() {
+    var id = parseInt(this.props.params.id, 10);
+    return {
+      user : UserStore.get(id) || {},
+      chirps : ChirpStore.byUserId(id),
+    }
+  },
+  componentDidMount : function() {
+    UserStore.addChangeListener(this.onChange);
+    ChirpStore.addChangeListener(this.onChange);
+  },
+  componentWillUnmount : function() {
+    UserStore.removeChangeListener(this.onChange);
+    ChirpStore.removeChangeListener(this.onChange);
+  },
+  onChange : function() {
+    this.setState(this.getInitialState());
+  },
+  render : function() {
+    console.log('UserProfile Rendering');
+    var chirps = this.state.chirps.map(function(chirp) {
+      return (
+        <li key={chirp.cid}> {chirp.text} </li>
+      );
+    })
+    return (
+      <div>
+        <img className='two columns' src={utils.avatar(this.state.user.email)}/>
+        <div className='ten columns'>
+          <h1> {this.state.user.fullname} </h1>
+          <h3 className='timestamp'> {this.state.user.username} </h3>
+
+          <p> <FollowButton userId={this.state.user.cid}/></p>
+
+          <ul>
+            {chirps}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+})
