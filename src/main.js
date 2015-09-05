8699cbc0d52125e0e66ddd636606feb468173fea
drkchiloll <samwomack_@me.com>
2015-09-05 12:33:30 -0500

    change /user/:id route to point to UserProfile component

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ var API = require('./api');
 var routes = (<Route handler={require('./components/App')}>
                 <Route name='home' path='/' handler={require('./components/Home')}/>
                 <Route name='users' handler={require('./components/UserList')}/>
-                <Route name='user' path='/user/:id' handler={require('./components/Home')}/>
+                <Route name='user' path='/user/:id' handler={require('./components/UserProfile')}/>
               </Route>
              );
 
